3fbf63989985491f7bd1d6574ef7767edd348782
Aysha Shafi <ayshaashafi003@gmail.com>
2026-06-22 13:13:45 +0530

    Add Google Analytics

--- src/analytics.js
+++ src/analytics.js
@@ -1,7 +1,12 @@
 import ReactGA from "react-ga4";
 
 const initGA = () => {
-  ReactGA.initialize("G-YQ3K0959XM"); 
+  ReactGA.initialize("G-YQ3K0959XM");
+
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
 };
 
 export default initGA;
